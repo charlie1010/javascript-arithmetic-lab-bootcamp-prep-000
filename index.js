fcde5ac7031004282ee1eea585ef4d85c4f84314
CHARLIE MCCORMICK <chamccor9692@gmail.com>
2018-04-18 19:19:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-function add() {
-  return a + b
+function add(a,b) {
+  return a+b
+}
+function subtract(a,b) {
+  return a-b
+}
+function divide(a,b) {
+  return a/b
+}
+function multiply(a,b) {
+  return a*b
+}
+function inc(n) {
+  return a+1
+}
+function decrease(n) {
+  return a-1
 }
